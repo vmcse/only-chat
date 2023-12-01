fa5f124d05d9a8fc52c12d5870072e9602dc67f7
Vamsi Madhav <vmcse09@gmail.com>
2023-12-01 20:13:52 +0530

    home and login pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import ForumRoundedIcon from "@mui/icons-material/ForumRounded";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Row>
+      <Col
+        md={6}
+        className="d-flex flex-direction-column align-items-center justify-content-center"
+      >
+        <div>
+          <h1>fjnvejvbnehjbnvejh</h1>
+          <p>vjnjervbnrhbnvrvbn</p>
+          <LinkContainer to="/chat">
+            <Button variant="success">
+              Get Started
+              <ForumRoundedIcon className="home-message-icon" />
+            </Button>
+          </LinkContainer>
+        </div>
+      </Col>
+      <Col md={6} className="home__bg"></Col>
+    </Row>
+  );
 };
 
 export default Home;

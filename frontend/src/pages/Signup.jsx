2906e1c9a8ff1fb6a6b70a1c95f7cea437898d9d
Vamsi Madhav <vmcse09@gmail.com>
2023-12-03 03:11:03 +0530

    added signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Signup.css";
+import pic from "../assets/default_profile_pic.jpg";
+import AddIcon from "@mui/icons-material/Add";
+
+const validateImg = () => {};
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <Container>
+      <Row>
+        <Col
+          md={7}
+          className="d-flex align-items-center justify-content-center flex-direction-column"
+        >
+          <Form style={{ width: "80%", maxWidth: 500 }}>
+            <h1 className="text-center">Create Account</h1>
+            <div className="signup-profile-pic__container">
+              <img src={pic} className="signup-profile-pic" />
+              <label htmlFor="image-upload" className="image-upload-label">
+                <AddIcon className="add-pic-icon" />
+              </label>
+              <input
+                type="file"
+                id="image-upload"
+                hidden
+                accept="image/png, image/jpeg"
+                onChange={validateImg}
+              />
+            </div>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Your name" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Create Account
+            </Button>
+            <div className="py-4">
+              <p className="text-center">
+                Already have an account? <Link to="/login">Login</Link>
+              </p>
+            </div>
+          </Form>
+        </Col>
+        <Col md={5} className="signup__bg"></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Signup;
